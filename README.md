828eaa933316136f7b2a68c0e71b044aac3611ef
cas <casska@gmail.com>
2014-12-18 10:53:45 -0600

    Init

---
+++ README.md
@@ -0,0 +1,10 @@
+#Globie Watcher
+
+Watcher to let others know when the Sync (btsync) folder has changed
+
+## Install
+```
+  npm install -g forever
+  npm install
+  forever start SYNC_DIR=path/to/sync/dir SLACK_API=slackapihere index.js
+```
---
+++ index.js
@@ -0,0 +1,58 @@
+var chokidar = require('chokidar');
+var SlackClient = require('slack-client');
+
+// Read environment vars
+var syncDirectory = process.env.SYNC_DIR;
+var slackApi = process.env.SLACK_API;
+
+// Set globieBot
+globieBot = new SlackClient(slackApi, true, false);
+globieBot.login();
+
+// Set watcher
+var watcher = chokidar.watch(syncDirectory, {
+  ignored: /\.!sync|\.sync|\.DS_Store/,
+  persistent: true,
+  ignoreInitial: true
+});
+
+watcher
+  .on('add', function(path) {
+    path = clearPath(path);
+    console.log('File', path, 'has been added');
+    sendMessage('File ' + path + ' has been added');
+  })
+  .on('addDir', function(path) {
+    path = clearPath(path);
+    console.log('Directory', path, 'has been added');
+    sendMessage('Directory ' + path + ' has been added');
+  })
+  .on('change', function(path) {
+    path = clearPath(path);
+    console.log('File', path, 'has been changed');
+    sendMessage('File ' + path + ' has been changed');
+  })
+  .on('unlink', function(path) {
+    path = clearPath(path);
+    console.log('File', path, 'has been removed');
+    sendMessage('File ' + path + ' has been removed');
+  })
+  .on('unlinkDir', function(path) {
+    path = clearPath(path);
+    console.log('Directory', path, 'has been removed');
+    sendMessage('Directory ' + path + ' has been removed');
+  })
+  .on('error', function(error) {console.error('Error happened', error);})
+  .on('ready', function() {console.info('Initial scan complete. Ready for changes.')})
+
+// clearPath()
+// Removes the sycnDirectory from the path to make it shorter
+var clearPath = function (path) {
+  path = path.replace(syncDirectory + "/", "");
+  return path;
+}
+
+var sendMessage = function(msg) {
+  channel = globieBot.getChannelByName('general');
+  channel.send(msg);
+}
